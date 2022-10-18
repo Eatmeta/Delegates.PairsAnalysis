aa9a79138edca96306c43632c7d91974c25322e1
Eatmeta <metagroul@gmail.com>
2022-10-18 13:23:05 +0300

    Initial commit

---
+++ Analysis.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delegates.PairsAnalysis
+{
+    public static class Analysis
+    {
+        public static int FindMaxPeriodIndex(params DateTime[] data)
+        {
+            return new MaxPauseFinder().Analyze(data);
+        }
+
+        public static double FindAverageRelativeDifference(params double[] data)
+        {
+            return new AverageDifferenceFinder().Analyze(data);
+        }
+    }
+}
